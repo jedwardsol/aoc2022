82d1dceadf19436f735f97abff519348a089f288
Jonathan Edwards <github@jonstuff.com>
2022-12-06 19:30:53 -0800

    da7 blank

---
+++ day07/day07.cpp
@@ -0,0 +1,27 @@
+#include "include/print.h"
+#include "include/thrower.h"
+#include "include/getdata.h"
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+
+int main()
+try
+{
+    for(auto const &line : getDataLines())
+    {
+    }
+}
+catch(std::exception const &e)
+{
+    print("{}",e.what());
+}
+
+
+
+// --------------------------
+std::istringstream testInput{
+R"(
+)"};
