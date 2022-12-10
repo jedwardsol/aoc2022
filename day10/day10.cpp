82bb460f1e6d95eae4324ded78b92a6afe7d2853
Jonathan Edwards <github@jonstuff.com>
2022-12-09 23:51:47 -0800

    day10 part1

--- day10/day10.cpp
+++ day10/day10.cpp
@@ -2,14 +2,70 @@
 #include "include/thrower.h"
 #include "include/getdata.h"
 
+#include <ranges>
+#include <functional>
 
+class CPU
+{
+public:
+    CPU(std::function<void(int,int)>    callback) : callback{std::move(callback)}
+    {
+    }
+
+    void run(std::ranges::forward_range auto &&program)
+    {
+        for(auto const &line : program)
+        {
+            if(line.starts_with("noop"))
+            {
+                tick();
+            }
+            else if(line.starts_with("addx"))
+            {
+                tick();
+                tick();
+                auto increment = std::stoi(line.substr(4));
+                x+=increment;
+            }
+        }
+    }
+
+private:
+
+    void tick()
+    {
+        callback(cycle,x);
+        cycle++;
+    }
+
+
+    int                             x    {1};
+    int                             cycle{1};
+    std::function<void(int,int)>    callback;
+
+};
 
 int main()
 try
 {
-    for(auto const &line : getDataLines())
+
+    int     part1Sum{};
+    auto    part1=[&](int cycle, int x)
     {
-    }
+        if(((cycle-20) % 40) == 0)
+        {
+//            print("{}*{}={}\n",cycle,x,cycle*x);
+
+            part1Sum+=cycle*x;
+        }
+    };
+
+    CPU     cpu{part1};
+
+    cpu.run(getDataLines());    
+
+
+    print("Part 1 : {}\n",part1Sum);
 }
 catch(std::exception const &e)
 {
@@ -21,5 +77,150 @@ catch(std::exception const &e)
 
 // --------------------------
 std::istringstream testInput{
-R"(
+R"(addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
 )"};
