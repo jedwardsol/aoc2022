2210a29577ee9cdade679e1fa8b9966bb1c016ed
Jonathan Edwards <github@jonstuff.com>
2022-12-11 22:56:52 -0800

    day12 part 2

--- day12/day12.cpp
+++ day12/day12.cpp
@@ -26,7 +26,7 @@ struct Search
 template<typename T>
 struct Grid
 {
-    Grid(int width, int height) : width{width},height{height},data(width*height)
+    Grid(int width, int height, T  init=T{}) : width{width},height{height},data(width*height, init)
     {}
 
     auto operator[](int row)
@@ -35,7 +35,18 @@ struct Grid
                             data.begin()+((row+1)*width)};
     }
 
+    auto operator[](int row) const
+    {
+        return std::span<T const>{data.begin()+(row*width),
+                                  data.begin()+((row+1)*width)};
+    }
+
     auto &operator[](Pos pos)
+    {
+        return data[ pos.row*width + pos.col];
+    }
+
+    auto &operator[](Pos pos) const
     {
         return data[ pos.row*width + pos.col];
     }
@@ -57,7 +68,7 @@ struct Candidate
 };
 
 
-std::vector< Pos > getNeighbours( Grid<int>  &terrain, Pos here )
+std::vector< Pos > getPart1Neighbours( Grid<int>  const &terrain, Pos here )
 {
     std::vector< Pos > neighbours;
 
@@ -97,47 +108,14 @@ std::vector< Pos > getNeighbours( Grid<int>  &terrain, Pos here )
 }
 
 
-
-int main()
-try
+// find shortest distance from start to end.  Can't climb more than 1 step up
+int solvePart1(Grid<int>    const &terrain, Pos const start, Pos const end)
 {
-    auto const data = getDataLines();
-
-    Grid<int>       terrain{static_cast<int>(data[0].size()), static_cast<int>(data.size())};
-    Grid<Search>    search {static_cast<int>(data[0].size()), static_cast<int>(data.size())};
-
-    Pos         start{};
-    Pos         end{};
-
-    for(int row=0;row<data.size();row++)
-    {
-        auto &line = data[row];
-
-        for(int column=0;column<line.size();column++)
-        {
-            auto c = line[column];
-
-            if(c=='S')
-            {
-                c='a';
-                start=Pos{row,column};
-                search[row][column].distance=0;
-            }
-            else if(c=='E')
-            {
-                c='z';
-                end=Pos{row,column};
-            }
-
-            terrain[row][column]=c-'a';
-        }
-    }
-
-
-    std::priority_queue<Candidate> fringe;
+    Grid<Search>                    search {terrain.width, terrain.height};
+    std::priority_queue<Candidate>  fringe;
     
+    search[start].distance=0;
     fringe.push( Candidate { 0, start} );
-
 
     while( !fringe.empty() ) 
     {
@@ -156,8 +134,7 @@ try
             break;
         }
 
-
-        auto neighbours  = getNeighbours( terrain, current.pos );
+        auto neighbours  = getPart1Neighbours( terrain, current.pos );
         auto newDistance = search[current.pos].distance + 1;
 
         for( auto neighbour : neighbours ) 
@@ -171,7 +148,134 @@ try
         }
     }
 
-    print("Part 1 : {}\n", search[end].distance);
+    return search[end].distance;
+}
+
+std::vector< Pos > getPart2Neighbours( Grid<int>  const &terrain, Pos here )
+{
+    std::vector< Pos > neighbours;
+
+    auto const heightHere = terrain[here];            
+
+    // L
+    if(   here.col > 0
+       && terrain[here.row][here.col-1] >= heightHere-1  )  
+    {
+        neighbours.emplace_back( here.row, here.col - 1 );
+    }
+
+    // R
+    if(   here.col < terrain.width-1
+       && terrain[here.row][here.col+1] >= heightHere-1  )  
+    {
+        neighbours.emplace_back( here.row, here.col +1  );
+    }
+
+
+    // U
+    if(   here.row > 0
+       && terrain[here.row-1][here.col] >= heightHere-1  )  
+    {
+        neighbours.emplace_back( here.row-1, here.col  );
+    }
+
+    //D
+    if(   here.row < terrain.height-1
+       && terrain[here.row+1][here.col] >= heightHere-1  )  
+    {
+        neighbours.emplace_back( here.row+1, here.col  );
+    }
+
+
+    return neighbours;
+}
+
+
+
+// find shortest distance from end to any height=0.  Can't climb more than 1 step up
+int solvePart2(Grid<int>    const &terrain, Pos const end)
+{
+    Grid<Search>                    search {terrain.width, terrain.height};
+    std::priority_queue<Candidate>  fringe;
+    
+    search[end].distance=0;
+    fringe.push( Candidate { 0, end} );
+
+    while( !fringe.empty() ) 
+    {
+        auto const current = fringe.top();
+        fringe.pop();
+
+        if( search[current.pos].visited) 
+        {
+            continue;
+        }
+
+        search[current.pos].visited = true;
+
+        if( terrain[current.pos] == 0 ) 
+        {
+            return search[current.pos].distance;
+        }
+
+        auto neighbours  = getPart2Neighbours( terrain, current.pos );
+        auto newDistance = search[current.pos].distance + 1;
+
+        for( auto neighbour : neighbours ) 
+        {
+            if( newDistance < search[neighbour].distance ) 
+            {
+                search[neighbour].distance = newDistance;
+
+                fringe.push( Candidate { newDistance, neighbour } );
+            }
+        }
+    }
+
+    throw_runtime_error("failed to find a route");
+}
+
+
+
+int main()
+try
+{
+    auto const  data{getDataLines()};
+
+    Grid<int>   terrain{static_cast<int>(data[0].size()), static_cast<int>(data.size())};
+
+    Pos         start{};
+    Pos         end{};
+
+    for(int row=0;row<data.size();row++)
+    {
+        auto &line = data[row];
+
+        for(int column=0;column<line.size();column++)
+        {
+            auto c = line[column];
+
+            if(c=='S')
+            {
+                c='a';
+                start=Pos{row,column};
+            }
+            else if(c=='E')
+            {
+                c='z';
+                end=Pos{row,column};
+            }
+
+            terrain[row][column]=c-'a';
+        }
+    }
+
+
+    auto part1 = solvePart1(terrain,start,end);
+    auto part2 = solvePart2(terrain,end);
+
+    print("Part 1 : {}\n", part1);
+    print("Part 2 : {}\n", part2);
 
 }
 catch(std::exception const &e)
