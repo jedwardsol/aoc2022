58a3a27ed269b4ca46fe3cbe54038954822af3d4
Jonathan Edwards <jonathae@cisco.com>
2022-12-12 14:27:16 -0800

    BFS

--- day12/search.cpp
+++ day12/search.cpp
@@ -11,16 +11,33 @@
 #include "day12.h"
 
 
+using Queue = std::queue<Candidate>;            // BFS
+//using Queue = std::priority_queue<Candidate>;   // Dijkstra
+
+
+Candidate pop(std::queue<Candidate> &queue)
+{
+    auto ret = queue.front();
+    queue.pop();
+    return ret;
+}
+
+Candidate pop(std::priority_queue<Candidate> &queue)
+{
+    auto ret = queue.top();
+    queue.pop();
+    return ret;
+}
 
 
 std::vector< Pos > getPart1Neighbours( Grid<int>  const &terrain, Pos here )
 {
     std::array<Vector,4> const   moves
     {{
-        {-1, 0},
-        {+1, 0},
-        { 0,-1},
         { 0,+1},
+        {+1, 0},
+        {-1, 0},
+        { 0,-1},
     }};
 
 
@@ -51,7 +68,7 @@ std::vector< Pos > getPart1Neighbours( Grid<int>  const &terrain, Pos here )
 std::pair<int,double> solvePart1(Grid<int>    const &terrain, Pos const start, Pos const end)
 {
     Grid<Search>                    search {terrain.width, terrain.height};
-    std::priority_queue<Candidate>  fringe;
+    Queue                           fringe;
 
     jle::stopwatch                  stopwatch{};
     
@@ -60,8 +77,7 @@ std::pair<int,double> solvePart1(Grid<int>    const &terrain, Pos const start, P
 
     while( !fringe.empty() ) 
     {
-        auto const current = fringe.top();
-        fringe.pop();
+        auto const current = pop(fringe);
 
         if( search[current.pos].visited) 
         {
@@ -156,7 +172,8 @@ std::vector< Pos > getPart2Neighbours( Grid<int>  const &terrain, Pos here )
 std::pair<int,double> solvePart2(Grid<int>    const &terrain, Pos const end)
 {
     Grid<Search>                    search {terrain.width, terrain.height};
-    std::priority_queue<Candidate>  fringe;
+    Queue                           fringe;
+
     jle::stopwatch                  stopwatch{};
 
     
@@ -165,8 +182,7 @@ std::pair<int,double> solvePart2(Grid<int>    const &terrain, Pos const end)
 
     while( !fringe.empty() ) 
     {
-        auto const current = fringe.top();
-        fringe.pop();
+        auto const current = pop(fringe);
 
         if( search[current.pos].visited) 
         {
