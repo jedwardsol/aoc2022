78fe04920a8c17561e31144969851c87b957fee2
Jonathan Edwards <jonathae@cisco.com>
2022-12-20 09:15:30 -0800

    day20

--- day20/day20.cpp
+++ day20/day20.cpp
@@ -4,71 +4,156 @@
 #include "include/getdata.h"
 
 
-struct Number
+// Horrific
+// TODO : get the vector version working.
+
+struct Element
 {
-    int index;
-    int value;
+    int     index;
+    int64_t value;
+
+    Element *flink;
+    Element *blink;
+
+    void insertBefore(Element   *element)
+    {
+        element->flink=this;
+        element->blink=blink;
+        blink->flink  =element;
+        blink         =element;
+    }
 };
+
+Element *findIndex(Element *walk, int index)
+{
+    while(walk->index!=index)
+    {
+        walk=walk->flink;
+    }
+    return walk;
+}
+
+Element *findValue(Element *walk, int64_t value)
+{
+    while(walk->value!=value)
+    {
+        walk=walk->flink;
+    }
+    return walk;
+}
+
+
+void print(Element const *walk)
+{
+    auto end=walk;
+    do
+    {
+        print("{:2} ",walk->value);
+        walk=walk->flink;
+    }while(walk!=end);
+}
+
+
+auto decrypt(Element *head, int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        auto walk       = findIndex(head,i);
+        auto distance   = walk->value;
+
+        distance %= (size-1);
+
+
+        if(distance < 0)
+        {
+            while(distance)
+            {
+                std::swap(walk->value, walk->blink->value);
+                std::swap(walk->index, walk->blink->index);
+                walk=walk->blink;
+                distance++;
+            }
+        }
+        else
+        {
+            while(distance)
+            {
+                std::swap(walk->value, walk->flink->value);
+                std::swap(walk->index, walk->flink->index);
+                walk=walk->flink;
+                distance--;
+            }
+        }
+    }
+}
+
+int64_t calculateCoords(Element *head)
+{
+    auto walk = findValue(head,0);
+
+    auto sum=0ll;   
+    for(int i=0;i<=3000;i++)
+    {
+        if(i == 1000 || i==2000||i==3000)
+        {
+            sum+=walk->value;
+        }
+
+        walk=walk->flink;
+    }
+
+    return sum;
+}
+
+
+void part1(std::vector<int> &numbers)
+{
+    Element  anchor{0, numbers[0], &anchor,&anchor};
+
+    Element *head(&anchor);
+
+    for(int i=1;i<numbers.size();i++)
+    {
+        head->insertBefore(new Element{i,numbers[i]});
+    }
+
+    decrypt(head,static_cast<int>(numbers.size()));
+    auto coords = calculateCoords(head);
+
+    print("Part 1 : {}\n",coords);
+}
+
+
+void part2(std::vector<int> &numbers)
+{
+    Element  anchor{0, numbers[0]*811589153ll, &anchor,&anchor};
+
+    Element *head(&anchor);
+
+    for(int i=1;i<numbers.size();i++)
+    {
+        head->insertBefore(new Element{i,numbers[i]*811589153ll});
+    }
+
+    for(int i=0;i<10;i++)
+    {
+        decrypt(head,static_cast<int>(numbers.size()));
+    }
+
+    auto coords = calculateCoords(head);
+
+    print("Part 2 : {}\n",coords);
+}
+
+
 
 int main()
 try
 {
-    auto numbers = []
-    {
-        std::vector<Number> numbers;
-        auto data = getDataLines(TestData{});
-        for(int i=0;i<data.size();i++)
-        {
-            numbers.emplace_back(i,std::stoi(data[i]));
-        }
-        return numbers;
-    }();
+    auto numbers{getDataInts()};
 
-
-    for(int i=0;i<numbers.size();i++)
-    {
-        auto    start   = std::distance(numbers.begin(), std::ranges::find(numbers,i,&Number::index));
-        auto    element = numbers[start];
-
-        auto    move    = element.value;
-
-        while(move < -(int)numbers.size())
-        {
-            move+=numbers.size();
-        }
-        while(move > (int)numbers.size())
-        {
-            move-=numbers.size();
-        }
-        
-        if(move+start <= 0)
-        {
-            move+=numbers.size()-1;            
-        }
-
-        auto    end     = start+move;
-
-        if(end >= numbers.size())
-        {
-            move-=numbers.size();
-            end -=numbers.size();
-        }
-
-        auto direction = (end >= start) ? 1 : -1;
-
-        while(move)
-        {
-            std::swap(numbers[start],numbers[start+direction]);
-            start += direction;
-            move  -= direction;
-        }
-
-
-        print("Moved the {:2} : ",element.value);
-        for(auto &e:numbers) print("{:2} ",e.value);
-        print("\n");
-
-    }
+    part1(numbers);
+    part2(numbers);
 }
 catch(std::exception const &e)
 {
@@ -86,5 +171,4 @@ R"(1
 3
 -2
 0
-4
-)"};
+4)"};
